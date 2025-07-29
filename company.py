87e4bc9d8a59e76eb4962d8ef4bfc64222ba2d74
Adarsh Sanjeet Kumar Singh <adarshsinghsingh488@gmail.com>
2025-07-29 18:42:54 +0530

    Update company.py

--- company.py
+++ company.py
@@ -4,7 +4,7 @@ import mysql.connector
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="123Andhapaisa@"
+    password="787787"
     # database yahan mention nahi karna
 )
 
